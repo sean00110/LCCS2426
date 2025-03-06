1a88808349f2f8040ee301fb644eba1bd76165c5
sean00110 <seanmurphyeire@gmail.com>
2025-03-06 09:13:18 +0000

    Add files via upload

---
+++ devBioJS.js
@@ -0,0 +1,11 @@
+//This JavaScript file will animate your Developer Bio page.
+
+//Welcome message.
+//Defining the function.
+function welcome(){
+    alert("Welcome to my page!");
+}
+//Calling the function
+welcome();
+
+//
